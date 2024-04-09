caba4e1e85974d71196c046d940fa61adfa67b8d
friend-of-your-mom <164667510+friend-of-your-mom@users.noreply.github.com>
2024-04-10 01:07:10 +0200

    Project name changed
    Added ability to save part of the file

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
-# Bit Editor
+# Zx Bitmap Editor
 
-Bit Editor is a web-based application initially designed primarily for editing binary files intended for the ZX Spectrum computer. However, it is versatile enough to be applied to any binary file editing tasks. The software provides a visual representation of the binary data, allowing users to view and modify individual bits easily.
+Zx Bitmap Editor is a web-based application initially designed primarily for editing binary files intended for the ZX Spectrum computer. However, it is versatile enough to be applied to any binary file editing tasks. The software provides a visual representation of the binary data, allowing users to view and modify individual bits easily.
 
 ## Features
 
@@ -12,19 +12,26 @@ Bit Editor is a web-based application initially designed primarily for editing b
 - Navigate through large files with an offset control feature.
 - Save modified binary files back to your local storage.
 
+## How to use
+
+1. Load a binary file
+2. View and modify bits. Left click to toggle bit on, right click to toggle bit off.
+3. Change the value of the Offset field using the keyboard or mouse to navigate through the loaded data.
+4. Save the modified file
+
 ## Disclaimer
 
-The author of Bit Editor does not assume any responsibility for the functionality of this software or any potential damage that may arise from its use. Users should utilize Bit Editor at their own risk.
+The author of Zx Bitmap Editor does not assume any responsibility for the functionality of this software or any potential damage that may arise from its use. Users should utilize Zx Bitmap Editor at their own risk.
 
 ## License
 
-Bit Editor's code is open for anyone to use, modify, and distribute without restrictions. Feel free to contribute to the project or use it as a basis for your own developments.
+Zx Bitmap Editor's code is open for anyone to use, modify, and distribute without restrictions. Feel free to contribute to the project or use it as a basis for your own developments.
 
 ## Usage
 
-To start using Bit Editor, simply load a binary file through the interface. You can then navigate through the file, make necessary bit alterations, and save the modified file to your device. The offset feature and bits per row slider allow for efficient navigation and editing of specific sections of the file.
+To start using Zx Bitmap Editor, simply load a binary file through the interface. You can then navigate through the file, make necessary bit alterations, and save the modified file to your device. The offset feature and bits per row slider allow for efficient navigation and editing of specific sections of the file.
 
 ## Contributing
 
-Contributions to Bit Editor are welcome. Whether it's feature requests, bug reports, or code contributions, your input is valuable in making Bit Editor a more powerful and user-friendly tool.
+Contributions to Zx Bitmap Editor are welcome. Whether it's feature requests, bug reports, or code contributions, your input is valuable in making Zx Bitmap Editor a more powerful and user-friendly tool.
 
--- script.js
+++ script.js
@@ -5,72 +5,124 @@ let originalFileName = '';
 let offset = 0;
 let maxLines = 800;
 
-document.getElementById('offsetInput').addEventListener('input', function(event) {
-    offset = parseInt(event.target.value) || 0;
-    syncOffset();
-    drawBits();
-});
+document.addEventListener('DOMContentLoaded', function() {
+    var saveModal = new bootstrap.Modal(document.getElementById('saveModal'));
 
-document.getElementById('offsetSlider').addEventListener('input', function(event) {
-    offset = parseInt(event.target.value) || 0;
-    document.getElementById('offsetInput').value = offset;
-    drawBits();
-});
+    document.getElementById('saveButton').addEventListener('click', function() {
+      const offsetInput = document.getElementById('saveModalOffset');
+      offsetInput.value = offset;
+      offsetInput.min = 0;
+      offsetInput.max = byteArray.length > 0 ? byteArray.length - 1 : 0; 
+      document.getElementById('saveModalLength').value = byteArray.length - offset;
+      saveModal.show();
+    });
 
-document.getElementById('fileInput').addEventListener('change', function(event) {
-    const file = event.target.files[0];
-    originalFileName = file.name;
-    const reader = new FileReader();
-    reader.onload = function(event) {
-        byteArray = new Uint8Array(event.target.result);
+    document.getElementById('saveModalOffset').addEventListener('input', function() {
+        const fromOffset = parseInt(this.value, 10) || 0;
+        document.getElementById('saveModalLength').value = byteArray.length - fromOffset;
+    });
+
+    document.getElementById('modalSaveButton').addEventListener('click', function() {
+        const offset = parseInt(document.getElementById('saveModalOffset').value, 10) || 0;
+        const length = parseInt(document.getElementById('saveModalLength').value, 10) || byteArray.length;
+        const partOfArray = byteArray.slice(offset, offset + length);
+        const blob = new Blob([partOfArray], {type: "application/octet-stream"});
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = originalFileName || 'modified_file.bin';
+        a.click();
+        // close modal after saving
+        const saveModal = bootstrap.Modal.getInstance(document.getElementById('saveModal'));
+        saveModal.hide();
+    });
+
+    document.getElementById('offsetInput').addEventListener('input', function(event) {
+        offset = parseInt(event.target.value) || 0;
         syncOffset();
         drawBits();
-    };
-    reader.readAsArrayBuffer(file);
+    });
+    
+    document.getElementById('offsetSlider').addEventListener('input', function(event) {
+        offset = parseInt(event.target.value) || 0;
+        document.getElementById('offsetInput').value = offset;
+        drawBits();
+    });
+    
+    document.getElementById('fileInput').addEventListener('change', function(event) {
+        const file = event.target.files[0];
+        originalFileName = file.name;
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            byteArray = new Uint8Array(event.target.result);
+            resetSettings();
+            syncOffset();
+            drawBits();
+            document.getElementById('saveButton').disabled = false;
+        };
+        reader.readAsArrayBuffer(file);
+    });
+    
+    document.getElementById('bitsPerRowSlider').addEventListener('input', function(event) {
+        bitsPerRow = parseInt(event.target.value);
+        document.getElementById('bitsPerRowValue').textContent = bitsPerRow / 8;
+        drawBits();
+    });
+    
+    document.getElementById('scaleSlider').addEventListener('input', function(event) {
+        scale = parseInt(event.target.value);
+        document.getElementById('scaleValue').textContent = scale;
+        drawBits();
+    });
+    
+    document.getElementById('canvas').addEventListener('mousedown', function(event) {
+        event.preventDefault();
+        modifyBit(event, event.button !== 2);
+    });
+    
+    document.getElementById('canvas').addEventListener('mousemove', function(event) {
+        if (event.buttons !== 0) {
+            modifyBit(event, event.buttons !== 2);
+        }
+    });
+
+    document.getElementById('showReadme').addEventListener('click', function(event) {
+        event.preventDefault();
+        fetch('README.md')
+          .then(response => response.text())
+          .then(text => {
+            document.getElementById('readmeContainer').innerHTML = marked.parse(text);
+            document.getElementById('readmePopup').style.display = 'block';
+          })
+          .catch(error => console.error('Error loading README.md:', error));
+    });
+
+    document.getElementById('canvas').addEventListener('contextmenu', event => event.preventDefault());
+
+    window.addEventListener('beforeunload', function (e) {
+        e.preventDefault();
+        e.returnValue = '';
+        return 'Are you sure you want to leave? You will lose all unsaved data.';
+    });
 });
 
-document.getElementById('bitsPerRowSlider').addEventListener('input', function(event) {
-    bitsPerRow = parseInt(event.target.value);
+function resetSettings() {
+    bitsPerRow = 8;
+    scale = 2;
+    offset = 0;
+    document.getElementById('bitsPerRowSlider').value = bitsPerRow;
     document.getElementById('bitsPerRowValue').textContent = bitsPerRow / 8;
-    drawBits();
-});
-
-document.getElementById('scaleSlider').addEventListener('input', function(event) {
-    scale = parseInt(event.target.value);
+    document.getElementById('scaleSlider').value = scale;
     document.getElementById('scaleValue').textContent = scale;
-    drawBits();
-});
-
-document.getElementById('canvas').addEventListener('mousedown', function(event) {
-    event.preventDefault();
-    modifyBit(event, event.button !== 2);
-});
-
-document.getElementById('canvas').addEventListener('mousemove', function(event) {
-    if (event.buttons !== 0) {
-        modifyBit(event, event.buttons !== 2);
-    }
-});
-
-document.getElementById('canvas').addEventListener('contextmenu', event => event.preventDefault());
-
-document.getElementById('saveButton').addEventListener('click', function() {
-    const blob = new Blob([byteArray], {type: "application/octet-stream"});
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = originalFileName || 'modified_file.bin';
-    a.click();
-});
+    document.getElementById('offsetInput').value = offset;
+    document.getElementById('offsetSlider').value = offset;
+}
 
 function syncOffset() {
     const totalBytes = byteArray.length;
-    // Максимальное значение смещения должно позволять отображать хотя бы один байт
     const maxOffset = totalBytes > 0 ? totalBytes - 1 : 0;
     document.getElementById('offsetSlider').max = maxOffset;
     document.getElementById('offsetMax').textContent = maxOffset.toString();
-
-    // Обеспечиваем, что текущее значение смещения не выходит за пределы допустимого диапазона
     offset = Math.min(offset, maxOffset);
     document.getElementById('offsetSlider').value = offset;
     document.getElementById('offsetInput').value = offset;
@@ -80,15 +132,12 @@ function drawBits() {
     const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
     canvas.width = bitsPerRow * scale;
-    // Расчет высоты канваса с учетом смещения и максимальной высоты в строках
     const linesToDisplay = Math.ceil((byteArray.length - offset) * 8 / bitsPerRow);
     canvas.height = Math.min(maxLines, linesToDisplay) * scale;
 
     let x = 0, y = 0;
-    // Проходим по байтам, начиная с указанного смещения
     for (let i = offset; i < byteArray.length; i++) {
         for (let bit = 7; bit >= 0; bit--) {
-            // Прекращаем отрисовку, если достигнута максимальная высота
             if (y >= Math.min(maxLines, linesToDisplay)) break;
             ctx.fillStyle = (byteArray[i] & (1 << bit)) ? 'black' : 'white';
             ctx.fillRect(x * scale, y * scale, scale, scale);
@@ -106,16 +155,11 @@ function modifyBit(event, setBit) {
     const x = Math.floor((event.clientX - rect.left) / scale);
     const y = Math.floor((event.clientY - rect.top) / scale);
     const bitIndex = y * bitsPerRow + x;
-    // Учитываем смещение при расчете byteIndex
-    const byteIndex = Math.floor(bitIndex / 8) + offset; // Добавляем offset здесь
+    const byteIndex = Math.floor(bitIndex / 8) + offset;
     const bit = 7 - bitIndex % 8;
-
-    // Проверяем, что byteIndex не выходит за пределы массива
     if (byteIndex < byteArray.length) {
-        // Устанавливаем или сбрасываем бит в соответствии с действием пользователя
         if (setBit) byteArray[byteIndex] |= 1 << bit;
         else byteArray[byteIndex] &= ~(1 << bit);
-        drawBits(); // Перерисовываем канвас, чтобы отразить изменения
+        drawBits();
     }
 }
-
